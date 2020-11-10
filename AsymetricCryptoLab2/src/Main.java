9e05cc1fd21eb4bb6237da1c7d5b4001fac4742f
root <the.vaho1337@gmail.com>
2020-11-10 20:29:15 +0200

    lab2 final

--- AsymetricCryptoLab2/src/Main.java
+++ AsymetricCryptoLab2/src/Main.java
@@ -10,6 +10,8 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    //good article how to use java.net.* https://stackoverflow.com/questions/2793150/how-to-use-java-net-urlconnection-to-fire-and-handle-http-requests
+
     public static void encryption_using_webService(String text, int keySize){
         System.out.println("-----------------------------------");
         System.out.println("encryption_using_webService");
@@ -97,7 +99,7 @@ public class Main {
         }
     }
 
-    public static void send_public_key_to_everyone_and_recieve_ciphertext(){
+    public static void send_public_key_and_recieve_ciphertext(){
         System.out.println("-----------------------------------");
         System.out.println("send_public_key_to_everyone_and_recieve_ciphertext");
         RSA receiver=new RSA(128);
@@ -139,6 +141,127 @@ public class Main {
         System.out.println("My verification: "+verification);
     }
 
+    public static void webService_sign_message_and_i_verify(String message,int keySize){
+        System.out.println("-----------------------------------");
+        System.out.println("webService_sign_message_and_i_verify");
+        System.out.println("Message: "+message);
+        try {
+            CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+            URL urlToGetKey = new URL("http://asymcryptwebservice.appspot.com/rsa/serverKey?keySize="+keySize);
+            URLConnection connection = urlToGetKey.openConnection();
+            String responce=new String(connection.getInputStream().readAllBytes());
+            System.out.println("Server open key: "+responce);
+            Pattern regex=Pattern.compile("\"modulus\":\"([\\dABCDEF]*)\".*\"publicExponent\":\"([\\dABCDEF]*)\"");
+            Matcher matcher=regex.matcher(responce);
+            if(matcher.find()){
+                BigInteger modulus=new BigInteger(matcher.group(1),16);
+                BigInteger publicExponent=new BigInteger(matcher.group(2),16);
+                RSA verifier=new RSA(modulus);
+                URL urlSign=new URL("http://asymcryptwebservice.appspot.com/rsa/sign?" +
+                        "message="+new BigInteger(1,message.getBytes()).toString(16).toUpperCase() +
+                        "&type=BYTES");
+                connection=urlSign.openConnection();
+                responce=new String(connection.getInputStream().readAllBytes());
+                System.out.println("Server sign: "+responce);
+                regex=Pattern.compile("\"signature\":\"([\\dABCDEF]*)\"");
+                matcher=regex.matcher(responce);
+                if(matcher.find()){
+                    System.out.println("Signature: "+matcher.group(1));
+                    BigInteger sign=new BigInteger(matcher.group(1),16);
+                    var verifierData=verifier.verify(message.getBytes(),sign.toByteArray(),modulus.toByteArray(),publicExponent.toByteArray());
+                    System.out.println("Verification: "+verifierData);
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void send_key_using_webService(int senderKeyLength,int receiverKeySize,byte[] sharedKey){
+        System.out.println("-----------------------------------");
+        System.out.println("send_key_using_webService");
+        try {
+            CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+            URL urlToGetKey = new URL("http://asymcryptwebservice.appspot.com/rsa/serverKey?keySize="+receiverKeySize);
+            URLConnection connection= urlToGetKey.openConnection();
+            String responce=new String(connection.getInputStream().readAllBytes());
+            System.out.println(responce);
+
+            Pattern regex=Pattern.compile("\"modulus\":\"([\\dABCDEF]*)\".*\"publicExponent\":\"([\\dABCDEF]*)\"");
+            Matcher matcher=regex.matcher(responce);
+            if(matcher.find()){
+                BigInteger receiver_n=new BigInteger(matcher.group(1),16);
+                BigInteger receiver_e=new BigInteger(matcher.group(2),16);
+                System.out.println("Receivers n: "+matcher.group(1));
+                System.out.println("Receivers e: "+matcher.group(2));
+
+                RSA sender=new RSA(senderKeyLength);
+                System.out.println("sender e: "+sender.getPublicKey()[0]);
+                System.out.println("sender n: "+sender.getPublicKey()[1]);
+                var senderData=sender.sendKey(receiver_n.toByteArray(),receiver_e.toByteArray(),sharedKey);
+                URL urlReceive=new URL("http://asymcryptwebservice.appspot.com/rsa/receiveKey?" +
+                        "key="+senderData[0].toString(16).toUpperCase() +
+                        "&signature="+senderData[1].toString(16).toUpperCase() +
+                        "&modulus=" +sender.getPublicKey()[1].toString(16).toUpperCase()+
+                        "&publicExponent="+sender.getPublicKey()[0].toString(16).toUpperCase());
+                connection=urlReceive.openConnection();
+                responce=new String(connection.getInputStream().readAllBytes());
+                System.out.println(responce);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public static void receive_key_using_webService(int senderKeyLength,int receiverKeySize){
+        System.out.println("-----------------------------------");
+        System.out.println("receive_key_using_webService");
+
+        try {
+            CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
+            URL urlToGetKey = new URL("http://asymcryptwebservice.appspot.com/rsa/serverKey?keySize="+senderKeyLength);
+            URLConnection connection= urlToGetKey.openConnection();
+            String responce=new String(connection.getInputStream().readAllBytes());
+            System.out.println(responce);
+            Pattern regex=Pattern.compile("\"modulus\":\"([\\dABCDEF]*)\".*\"publicExponent\":\"([\\dABCDEF]*)\"");
+            Matcher matcher=regex.matcher(responce);
+            if (matcher.find()) {
+                BigInteger modulus_sender=new BigInteger(matcher.group(1),16);
+                BigInteger publicExpinent_sender=new BigInteger(matcher.group(2),16);
+                RSA receiver = new RSA(receiverKeySize);
+                URL urtToSendKey = new URL("http://asymcryptwebservice.appspot.com/rsa/sendKey?" +
+                        "modulus=" + receiver.getPublicKey()[1].toString(16).toUpperCase() +
+                        "&publicExponent=10001");
+                connection = urtToSendKey.openConnection();
+                responce = new String(connection.getInputStream().readAllBytes());
+                System.out.println(responce);
+                regex = Pattern.compile("\"key\":\"([\\dABCDEF]*)\".*\"signature\":\"([\\dABCDEF]*)\"");
+                matcher = regex.matcher(responce);
+                if (matcher.find()) {
+                    BigInteger k_1 = new BigInteger(matcher.group(1), 16);
+                    BigInteger s_1 = new BigInteger(matcher.group(2), 16);
+                    var receiverData = receiver.receiveKey(k_1, s_1, modulus_sender.toByteArray(),publicExpinent_sender.toByteArray());
+                    System.out.println("Shared key: "+receiverData[0]);
+                    System.out.println("Verification: "+receiverData[1]);
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     public static void send_key_and_receive_key(){
         System.out.println("-----------------------------------");
@@ -158,11 +281,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        encryption_using_webService("some text",128);
-        decryption_using_webService("some test");
-        send_public_key_to_everyone_and_recieve_ciphertext();
-        sign_message_and_verify_the_sign_using_webService("teext");
-        send_key_and_receive_key();
+          encryption_using_webService("some text",128);
+          decryption_using_webService("some test");
+          send_public_key_and_recieve_ciphertext();
+          sign_message_and_verify_the_sign_using_webService("teext");
+          webService_sign_message_and_i_verify("test",128);
+          send_key_using_webService(128,256,"secret".getBytes());//
+          receive_key_using_webService(128,256);
+          send_key_and_receive_key();
 
     }
 }
